c4029464df7e6b6f9a7ea7c11dad2a37d31b01d0
Satya Mehta <satya.h.mehta@gmail.com>
2022-04-28 14:26:36 -0500

    completed settings page design

--- my-blog/src/pages/settings/Settings.jsx
+++ my-blog/src/pages/settings/Settings.jsx
@@ -1,12 +1,34 @@
-import "./settings.css"
-import Sidebar from '../../componenets/sidebar/Sidebar'
+import "./settings.css";
+import Sidebar from "../../componenets/sidebar/Sidebar";
+import Satya from "./satya.png";
 
 export default function Settings() {
   return (
     <div className="settings">
-        <div className="settingsWrapper">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Your Account</span>
         </div>
-        <Sidebar />
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsProfilePicture">
+            <img src={Satya} alt="" />
+            <label htmlFor="fileInput">
+              <i class="settingsProfilePictureIcon fa-solid fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display:'none'}}/>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="satya" />
+          <label>Email</label>
+          <input type="email" placeholder="satya@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
     </div>
-  )
+  );
 }
